0895b151007c42798a3fb4032f19e751b5973647
David Busuru <davidbusuru1@gmail.com>
2025-10-23 20:16:15 +0300

    main

--- Stored Procedures.sql
+++ Stored Procedures.sql
@@ -11,15 +11,15 @@ select * from employee_salary
 call large_salaries();
 
 delimiter $$
-create procedure large_salaries2()
+create procedure large_salaries3()
 begin
 select * from employee_salary
 	where salary >= 50000;
 select * from employee_salary
 	where salary >= 100000;
 end $$
-delimiter ; large_salaries2large_salaries2
+delimiter ; 
 
-call large_salaries2();
+call large_salaries3();
 
     
