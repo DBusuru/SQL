e3e867beaffbf3348e18b2b23333e9984a0e3bf7
David Busuru <davidbusuru1@gmail.com>
2025-10-25 22:14:35 +0300

    main\

---
+++ select statments 2.sql
@@ -0,0 +1,5 @@
+-- select statments
+
+create table book (Title varchar(50), Edition varchar(100), year date, price float, ISBN numeric, pages varchar(20), Aisle varchar(20), Description varchar(100));
+
+insert into book values 
