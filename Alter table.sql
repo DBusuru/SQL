b8e2e35b4f5d856734f53b4410d0ef02633fd1bb
David Busuru <davidbusuru1@gmail.com>
2025-10-27 21:30:48 +0300

    main

---
+++ Alter table.sql
@@ -0,0 +1,7 @@
+-- Alter table
+
+alter table book
+	add column sales varchar(50);
+    
+alter table book	
+	modify pages char(50);
--- select statments 2.sql
+++ select statments 2.sql
@@ -2,4 +2,10 @@
 
 create table book (Title varchar(50), Edition varchar(100), year date, price float, ISBN numeric, pages varchar(20), Aisle varchar(20), Description varchar(100));
 
-insert into book values 
+insert into book values ('Database_Fundamentals', 1, '2010-01-01', 24.99, 978-0-9800628,300, 'DB-A01','Teaches you the fundamentals of databases');
+
+select * from book;
+
+select title, pages, description from book;
+
+select count(*) from book;
