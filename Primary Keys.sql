e27cd7231053050d40a8df663af32664d7757b19
David Busuru <davidbusuru1@gmail.com>
2025-10-28 19:28:28 +0300

    main

--- Primary Keys.sql
+++ Primary Keys.sql
@@ -8,3 +8,9 @@ create table Author(
     city varchar(15),
     country char(2)
 );
+
+insert into author values (
+	01,'Busuru', 'David','davidbusuru1@gmail.com', 'Nairobi', 'KE'),
+    ( 02, 'Nyambura', 'Janice', 'jancynyambu@gmail.com', 'Nairobi', 'KE');
+    
+select * from author;
